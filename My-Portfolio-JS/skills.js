41a70b8d9487c6dea415fa6c41e3a8c6085cf9f7
Jeansy Pena <jeansypenar@outlook.com>
2024-11-24 23:41:06 -0400

    updated skills.js logic

--- My-Portfolio-JS/skills.js
+++ My-Portfolio-JS/skills.js
@@ -2,48 +2,37 @@
 var frontEnd = document.getElementById('front-end');
 var backEnd = document.getElementById('back-end');
 var technologies = document.getElementById('technologies');
-// calling data from json file
+// Fetch data from json file
 const obtainSkills = () => {
-    axios({
-        method: 'GET',
-        url: 'My-Portfolio-JSON/skills.json'
-    }).then(res => {
+    axios.get('./My-Portfolio-JSON/skills.json')
+    .then(res => {
         skillsCards(res.data);
-    }).catch(err => {
-        console.log('Hubo un error al cargar los skills', err)
+    })
+    .catch(err => {
+        console.log('Error loading skills', err);
     });
 };
 
-const skillsCards = (art) => {
-    art.forEach(skill => {
-        if(skill.type === 'front-end') {
-            frontEnd.innerHTML += `
+const skillsCards = (skills) => {
+    skills.forEach(skill => {
+        const container = skill.type === 'front-end'
+            ? frontEnd
+            : skill.type === 'back-end'
+            ? backEnd
+            : skill.type === 'technologies'
+            ? technologies
+            : null;
+
+        if (container) {
+            container.innerHTML += `
                 <div class="card">
                     <img src="${skill.logo}" style="width: 9rem;" alt="${skill.tittle}">
-                        <div class="card-content">
-                            <h3>${skill.tittle}</h3>
-                                </div>
-                                    </div>`
-        } else if(skill.type === 'back-end') {
-            backEnd.innerHTML += `
-                <div class="card">
-                    <img src="${skill.logo}" style="width: 9rem;" alt="${skill.tittle}">
-                        <div class="card-content">
-                            <h3>${skill.tittle}</h3>
-                                </div>
-                                    </div>`
-        } else if(skill.type === 'technologies') {
-            technologies.innerHTML += `
-                <div class="card">
-                    <img src="${skill.logo}" style="width: 9rem;" alt="${skill.tittle}">
-                        <div class="card-content">
-                            <h3>${skill.tittle}</h3>
-                                </div>
-                                    </div>`
-        } else {
-            return;
+                    <div class="card-content">
+                        <h3>${skill.tittle}</h3>
+                    </div>
+                </div>`;
         }
-    })
-}
+    });
+};
 //call function and arrow the DOM variable.
 obtainSkills();
