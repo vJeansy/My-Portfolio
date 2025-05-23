969896851ebd88c06a108904bd1baf8e31c51de2
Jeansy Pena <jeansypenar@outlook.com>
2025-05-23 11:09:21 -0400

    input.css added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}" // Adjust paths based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
